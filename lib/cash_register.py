8c28d0ca8f7f3d6632c88d45db513eb9a870d558
Tabithaokoth <kobbitho@gmail.com>
2024-05-28 09:42:28 +0300

    lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self):
+        self.items = []
+        self.total = 0
+        self.last_transaction = 0
+
+    def add_item(self, item, quantity, price):
+        self.items.append((item, quantity, price))
+        self.total += quantity * price
+
+    def apply_discount(self, discount_percentage):
+        discount_amount = self.total * (discount_percentage / 100)
+        return self.total - discount_amount
+
+    def void_last_transaction(self):
+        if self.last_transaction > 0:
+            self.total -= self.last_transaction
+            self.last_transaction = 0
+
+    def get_items(self):
+        return self.items
+
+    def get_total(self):
+        return self.total
+
+    def get_last_transaction(self):
+        return self.last_transaction
